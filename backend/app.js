60951bb2488d97869ecc79b7e49dbc8c7af944d8
erika <bambam.gillig@gmail.com>
2022-02-22 11:30:48 -0300

    1.2.3

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var mysql = require('mysql');
+var cors = require('cors');
 
 
 require('dotenv').config();
@@ -17,6 +18,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
 var adminRouter = require('./routes/admin/novedades');
+var apiRouter = require('./routes/api');
 
 
 
@@ -88,6 +90,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
 app.use('/admin/novedades', secured, adminRouter);
+app.use('/api', cors(), apiRouter);
 
 
 
